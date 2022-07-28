0ad754e560f2d49c9f0b686ef2419d6388970fd1
Diana Mendoza <diana.pat.mendoza@gmail.com>
2022-07-28 02:18:30 -0500

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheightToCelsius(90);
+            Console.WriteLine($"celcius {celsius}");
+
+            var fahrenheight = TempConverter.CelsiusToFahrenheight(20);
+            Console.WriteLine($"fahrenheight {fahrenheight}");
+        }
+    }
+}
+
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheightToCelsius(double fahrenheight)
+        {
+            return (fahrenheight -32) / 1.8;
+        }
+        public static double CelsiusToFahrenheight(double celsius)
+        {
+            return (celsius * 9) / 5 + 32;
+        }
+    }
+}
+
